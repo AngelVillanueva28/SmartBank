6e72c28bf0c338169b870d093aa929677fa97e6b
Angel Villanueva <angeloox054@gmail.com>
2023-04-30 01:02:25 -0500

    Clase04 completa

---
+++ src/Components/List/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Btn } from "../UI";
+import { lista } from "../../info";
+import Card from "../Card";
+
+const List = () => {
+  return <Box>
+    {
+      lista.cargos.map( (cargo, index) => {
+        return <Card key={index} cargo={cargo}/>
+      })
+    }
+    <Btn>Ver más</Btn>
+  </Box>;
+}
+
+export default List;
